9b39b391514968b8062892f2ef6def07f67e3001
Joao Eduardo Luis <joao.luis@inktank.com>
2014-08-12 20:16:10 +0100

    Does Not Work: MLogAck
    Signed-off-by: Joao Eduardo Luis <joao.luis@inktank.com>

--- src/messages/MLogAck.h
+++ src/messages/MLogAck.h
@@ -15,36 +15,44 @@
 #ifndef CEPH_MLOGACK_H
 #define CEPH_MLOGACK_H
 
+#include "common/LogEntry.h"
 #include <uuid/uuid.h>
 
 class MLogAck : public Message {
 public:
   uuid_d fsid;
-  version_t last;
-  std::string channel;
+  version_t last; // keeping it for backward compatibility
+  std::map<std::string, version_t> channel_last;
 
-  MLogAck() : Message(MSG_LOGACK) {}
-  MLogAck(uuid_d& f, version_t l) : Message(MSG_LOGACK), fsid(f), last(l) {}
+  static const int HEAD_VERSION = 2;
+  static const int COMPAT_VERSION = 1;
+
+  MLogAck() : Message(MSG_LOGACK, HEAD_VERSION, COMPAT_VERSION) {}
+  MLogAck(uuid_d& f, version_t l, std::map<std::string, version_t> &m)
+    : Message(MSG_LOGACK, HEAD_VERSION, COMPAT_VERSION),
+      fsid(f), last(v), channel_last(m) {}
 private:
   ~MLogAck() {}
 
 public:
   const char *get_type_name() const { return "log_ack"; }
   void print(ostream& out) const {
-    out << "log(last " << last << ")";
+    out << "log(last " << channel_last << ")";
   }
 
   void encode_payload(uint64_t features) {
     ::encode(fsid, payload);
-    ::encode(last, payload);
-    ::encode(channel, payload);
+    ::encode(channel_last, payload);
   }
   void decode_payload() {
     bufferlist::iterator p = payload.begin();
     ::decode(fsid, p);
     ::decode(last, p);
-    if (!p.end())
-      ::decode(channel, p);
+    if (header.version < 2) {
+      channel_last[CLOG_CHANNEL_DEFAULT] = last;
+    } else {
+      ::decode(channel_last, p);
+    }
   }
 };
 
